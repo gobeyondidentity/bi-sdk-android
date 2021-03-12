9e5fdac3934850b7f348a42ca3481a8351e496ed
Igor Brishkoski <igor.brishkoski@beyondidentity.com>
2021-03-12 15:12:10 -0500

    Make changes to the auth view

--- app/src/main/java/com/beyondidentity/authenticator/sdk/android/SignInActivity.kt
+++ app/src/main/java/com/beyondidentity/authenticator/sdk/android/SignInActivity.kt
@@ -2,17 +2,17 @@ package com.beyondidentity.authenticator.sdk.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.beyondidentity.authenticator.sdk.AuthenticatorSignInButton
+import com.beyondidentity.authenticator.sdk.AuthView
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val logInButton = findViewById<AuthenticatorSignInButton>(R.id.my_login_button)
+//        val logInButton = findViewById<AuthView>(R.id.my_login_button)
 
-        logInButton.setPasswordlessLogInListener(
-            loginUrl = "$ACME_CLOUD_URL/start",
-            redirectUrl = "${getString(R.string.app_scheme)}://${getString(R.string.app_host)}",
-        )
+//        logInButton.setPasswordlessLogInListener(
+//            loginUrl = "$ACME_CLOUD_URL/start",
+//            redirectUrl = "${getString(R.string.app_scheme)}://${getString(R.string.app_host)}",
+//        )
     }
 }
