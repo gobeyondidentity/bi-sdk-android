b34261a2dde95c8d920441b9c42d9dbf25015d6f
Igor Brishkoski <igor.brishkoski@beyondidentity.com>
2021-03-18 12:27:53 -0400

    convert to gradle kts

---
+++ buildSrc/src/main/java/config/Library.kt
@@ -0,0 +1,47 @@
+package config
+
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+private typealias AndroidBaseExtension = BaseExtension
+
+fun Project.configureAndroidLib() = this.extensions.getByType<AndroidBaseExtension>().run {
+
+    compileSdkVersion(AndroidConfig.COMPILE_SDK_VERSION)
+    buildToolsVersion(AndroidConfig.BUILD_TOOLS_VERSION)
+
+    defaultConfig {
+        minSdkVersion(AndroidConfig.MIN_SDK_VERSION)
+        targetSdkVersion(AndroidConfig.TARGET_SDK_VERSION)
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    defaultConfig {
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+
+//    tasks.withType<KotlinCompile> {
+//        kotlinOptions {
+//            jvmTarget = "1.8"
+//        }
+//    }
+}
