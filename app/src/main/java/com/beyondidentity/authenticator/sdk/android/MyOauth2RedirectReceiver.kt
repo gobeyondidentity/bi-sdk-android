6f0fd681f5bf976c603e7e9ffdb941fbc3cdb2af
Igor Brishkoski <igor.brishkoski@beyondidentity.com>
2021-03-23 14:06:32 -0400

    Document both flows to be closer to iOS docs

--- app/src/main/java/com/beyondidentity/authenticator/sdk/android/MyOauth2RedirectReceiver.kt
+++ app/src/main/java/com/beyondidentity/authenticator/sdk/android/MyOauth2RedirectReceiver.kt
@@ -18,20 +18,28 @@ class MyOauth2RedirectReceiver : AppCompatActivity() {
     // The session is our access token, we can use to make API calls to
     // our acme cloud backend
     private fun extractSession(intent: Intent) {
+        lateinit var nextStepIntent: Intent
+
         intent.data?.getQueryParameter("session")?.let { session ->
             // we got the session, save it and go to home screen to start making network calls
             Log.d("MyOauth2RedirectReceiver", "We got our sessions $session")
             getSharedPreferences(DEFAULT_SHARED_PREFS_KEY, MODE_PRIVATE).edit {
                 putString(SHARED_PREF_SESSION_KEY, session)
             }
-            startActivity(Intent(this, HomeActivity::class.java))
+
+            nextStepIntent = getNextStepIntent(HomeActivity::class.java)
         } ?: run {
             // there was an error getting the session, go back to sign in screen
             Log.e("MyOauth2RedirectReceiver", "Error getting the session")
             Toast.makeText(this, "Error Signing In", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this, SignInActivity::class.java))
+            nextStepIntent = getNextStepIntent(SignInActivity::class.java)
         }
 
+        startActivity(nextStepIntent)
         finish()
+    }
+
+    private fun getNextStepIntent(clazz: Class<*>) = Intent(this, clazz).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
     }
 }
